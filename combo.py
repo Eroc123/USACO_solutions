92e38fe3f31c3f7f405df837ef116e45b8ee22c5
Eroc123 <79434990+Eroc123@users.noreply.github.com>
2021-08-30 21:13:08 +0700

    Update combo.py

--- combo.py
+++ combo.py
@@ -9,7 +9,7 @@ data = fin.read().split('\n')
 ns = []
 for i in range(1,int(data[0])+1):
     ns.append(i)
-
+print(ns)
 solutions = []
 #print(data)
 #print(int(data[1].split()[2])+2)
@@ -17,16 +17,16 @@ for i in range(int(data[1].split()[0])-3,int(data[1].split()[0])+2):
     for j in range(int(data[1].split()[1])-3,int(data[1].split()[1])+2):
         for k in range(int(data[1].split()[2])-3,int(data[1].split()[2])+2):
             try:
-                solutions.append(str([ns[i],ns[j],ns[k]]))
-            except:
-                pass
+                solutions.append(str([ns[i-2],ns[j-2],ns[k-2]]))
+            except Exception as e:
+                print(e, i,j,k)
 for i in range(int(data[2].split()[0])-3,int(data[2].split()[0])+2):
     for j in range(int(data[2].split()[1])-3,int(data[2].split()[1])+2):
         for k in range(int(data[2].split()[2])-3,int(data[2].split()[2])+2):
             try:
-                solutions.append(str([ns[i],ns[j],ns[k]]))
-            except:
-                pass
+                solutions.append(str([ns[i-2],ns[j-2],ns[k-2]]))
+            except Exception as e:
+                print(e, i,j,k)
 
 solutions = list(dict.fromkeys(solutions))
 #print(solutions)
