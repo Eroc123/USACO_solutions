8c3c69d2faf62d77fa4225dbf8203aec86e22a41
Eroc123 <79434990+Eroc123@users.noreply.github.com>
2021-09-05 11:29:44 +0700

    Update 1.4.2-barn1.py

--- 1.4.2-barn1.py
+++ 1.4.2-barn1.py
@@ -22,35 +22,13 @@ for i in range(int(occ)-1):
     spaces.append(int(cows[i+1]) - int(cows[i]))
 spaces.sort()
 largegaps =0
-print(cows)
 
-spaces = spaces[::-1]
-print(spaces)
-if int(m)-1 == 1:
-    largegaps = 1
 if int(m)-1 == 0:
     fout.write(str(max(cows)-min(cows)+1)+'\n')
     exit()
 for i in range(int(m)-1):
-    largegaps += (spaces[i])
-    print(spaces[i])
+    largegaps += spaces.pop()-1
+    print(largegaps)
 print(largegaps)
-fout.write(str(max(cows)-largegaps+1))
-fout.write('\n')
-exit()
-
-print(cows)
-outpt = 0
-if m > occ:
-    outpt = occ
-spaces = []
-for i in range(int(occ)-3):
-    spaces.append(int(cows[i+1]) - int(cows[i]) - 1)
-spaces.sort()
-print(spces)
-gap = 0
-for i in range(int(m)-1):
-    gap += spaces[i*-1]
-outpt = max(cows) - min(cows) + 1 - gap
-fout.write(str(outpt-3))
+fout.write(str(max(cows)-min(cows)-largegaps+1))
 fout.write('\n')
